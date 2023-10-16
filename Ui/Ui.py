a783ea3b2063bf781b914fcdeab983af316567e0
Erkagoon <renooo21@hotmail.fr>
2023-10-16 07:57:31 +0200

    Petite modif d'une faute

--- Ui/Ui.py
+++ Ui/Ui.py
@@ -115,7 +115,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Mind2Project"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Mind2Notes"))
         self.pushButton_4.setText(_translate("MainWindow", "PushButton"))
         self.pushButton_3.setText(_translate("MainWindow", "PushButton"))
         self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
