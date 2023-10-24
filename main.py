90485e947bd7d65512e18e8e972267d3d1b1fdd6
Erkagoon <renooo21@hotmail.fr>
2023-10-24 17:12:08 +0200

    Mise en arrière plan de l'application

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from PyQt6 import QtWidgets, QtCore
+from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtWidgets import QMenu, QSystemTrayIcon
+from PyQt6.QtCore import Qt
 from Ui import Ui  # Import du module ui
 from Ui.Overlay import Overlay  # Import du module ui
 from components.Record import Record
@@ -11,8 +14,31 @@ from components.CategoriesBtn import CategoriesBtn
 from components.ProjectsBtn import ProjectsBtn
 
 class MyMainWindow(QtWidgets.QMainWindow, Ui.Ui_MainWindow):
-    def __init__(self, projects_db, categories_db, *args, **kwargs):
+    def __init__(self, app, projects_db, categories_db, *args, **kwargs):
         super(MyMainWindow, self).__init__(*args, **kwargs)
+
+        self.app = app
+
+    ########### Mise en arrière plan de l'application #############
+        # Créez une icône pour l'application
+        icon_path = "icons/notebook.png"
+        self.tray_icon = QSystemTrayIcon(QIcon(icon_path), self)
+
+        # Créez un menu pour l'icône de plateau
+        self.tray_menu = QMenu(self)
+
+        show_action = QAction("Afficher", self)
+        show_action.triggered.connect(self.show_window)
+        self.tray_menu.addAction(show_action)
+
+        exit_action = QAction("Quitter", self)
+        exit_action.triggered.connect(self.app.quit)
+        self.tray_menu.addAction(exit_action)
+
+        self.tray_icon.activated.connect(self.on_tray_icon_activated)
+        self.tray_icon.setContextMenu(self.tray_menu)
+        self.tray_icon.show()
+    ###############################################################
 
         self.categories_db = categories_db
         self.setupUi(self)
@@ -36,23 +62,14 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui.Ui_MainWindow):
         #self.actionnew_record.triggered.connect(self.on_record_note_pressed)
         #self.myButton.pressed.connect(self.my_method)
 
-    # def retranslateUi(self, MainWindow):
-    #     # Appel à la méthode originale
-    #     Ui.Ui_MainWindow.retranslateUi(self, MainWindow)
+    def on_tray_icon_activated(self, reason):
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:  # Double-click
+            self.show()
 
-    #     _translate = QtCore.QCoreApplication.translate
-
-    #     # Votre code personnalisé pour les boutons de catégorie
-    #     if hasattr(self, "category_buttons") and self.category_buttons:
-    #         for button in self.category_buttons:
-    #             button_name = button.text()
-    #             button.setText(_translate("MainWindow", button_name))
-
-    #     # Votre code personnalisé pour les boutons de projet
-    #     if hasattr(self, "project_buttons") and self.project_buttons:
-    #         for button in self.project_buttons:
-    #             button_name = button.text()
-    #             button.setText(_translate("MainWindow", button_name))
+    def show_window(self):
+        self.show()
+        self.setWindowState(self.windowState() & ~Qt.WindowState.WindowMinimized | Qt.WindowState.WindowActive)
+        self.activateWindow()
 
     def refresh_categories(self, project_id):
         self.clear_layout(self.gridLayout_4)
@@ -96,6 +113,10 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui.Ui_MainWindow):
         function_call = vocal_commands.execute_command()
         print(f"Function call : {function_call}")
 
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+
     # def on_new_record_triggered(self):
     #     # action utilisant le bouton new_record
     #     self.recorder.start_record()
@@ -109,11 +130,11 @@ def main():
 
     ##################################### Exemple d'insert dans la db projectsAndCats ################################################
     # Liste des noms de projets à insérer
-    project_names = ["Chats", "Chiens", "Oiseaux", "Fourmis", "Cervidés", "Vaches", "Chevaux", "Animaux de la ferme"]
+    # project_names = ["Chats", "Chiens", "Oiseaux", "Fourmis", "Cervidés", "Vaches", "Chevaux", "Animaux de la ferme"]
 
     # Insérer chaque projet dans la base de données
-    for project_name in project_names:
-        projects_db.insert(project_name)
+    # for project_name in project_names:
+    #     projects_db.insert(project_name)
     ##################################################################################################################################
 
     ##################################### Exemple d'insert dans la db projectsAndCats ################################################
@@ -134,7 +155,7 @@ def main():
     #         categories_db.insert(category_name, project_id)
     ##################################################################################################################################
 
-    window = MyMainWindow(projects_db, categories_db)  # Utilisation de notre sous-classe personnalisée au lieu de QMainWindow
+    window = MyMainWindow(app, projects_db, categories_db)  # Utilisation de notre sous-classe personnalisée au lieu de QMainWindow
     window.show()
     app.exec()
 
